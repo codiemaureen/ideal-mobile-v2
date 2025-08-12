ede5315b37de7398a23ecf3b82f64736eec7158c
Codiemaureen <codiestover.cs@gmail.com>
2025-08-12 19:05:47 -0400

    Adding constants file

---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Ideal Mobile Detailing';
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Online platform for advertising and scheduling mobile detailing services';
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000/';
