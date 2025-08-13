6943fb521c825299f416f028be1303cc3fbc8adc
Codiemaureen <codiestover.cs@gmail.com>
2025-08-12 20:22:02 -0400

    Adding tailwind config

---
+++ tailwind.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [], // v4 generally doesn't need this, keep only if you know why
+  theme: { extend: {} },
+  plugins: [],
+};
+export default config;
