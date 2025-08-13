06751b1d502e742bbddad66f1bd1644d5946c9f5
Codiemaureen <codiestover.cs@gmail.com>
2025-08-13 12:32:48 -0400

    Adding loading and not found files

---
+++ app/loading.jsx
@@ -0,0 +1,22 @@
+import loader from '@/assets/loader.gif';
+import Image from 'next/image';
+
+const loadingPage = () => {
+  return (
+   <div style={{
+    display:'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    width: '100vw'
+   }}>
+    <Image 
+     src={loader} 
+     alt='loading'
+     height={150}
+     width={150} />
+   </div>
+  );
+}
+
+export default loadingPage;
