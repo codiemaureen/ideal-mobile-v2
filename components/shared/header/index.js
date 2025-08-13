14dabf88d8a55079e6cf3a2967a6e839cd872a6d
Codiemaureen <codiestover.cs@gmail.com>
2025-08-13 11:04:39 -0400

    Adding menu drawer to header

--- components/shared/header/index.js
+++ components/shared/header/index.js
@@ -1,15 +1,21 @@
 import { APP_NAME } from "@/lib/constants";
+import MenuDrawer from "./menu-drawer";
+
+
 
 const Header = () => {
- return ( 
-    <header className='w-full border-b bg-amber-500'>
-      <div className="wrapper flex-between text-3xl">
-        <span className="hidden lg:block font-bold text-2xl ml-3">
-          {APP_NAME}
-        </span>
-      </div>
-    </header>
-  );
+  return ( 
+      <header className='w-full border-b bg-amber-500'>
+        <div className="wrapper flex-between text-3xl">
+          <div className="flex-start">
+            <MenuDrawer />
+            <span className="hidden lg:block font-bold text-2xl ml-3">
+              {APP_NAME}
+            </span>
+          </div>
+        </div>
+      </header>
+    );
 }
- 
+
 export default Header;
