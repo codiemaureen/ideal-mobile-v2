626fd84d522ec86bc49ac152b1b0a613232ae4a4
Codiemaureen <codiestover.cs@gmail.com>
2025-08-13 11:36:49 -0400

    Adding options to menu drawer

--- components/shared/header/menu-drawer.jsx
+++ components/shared/header/menu-drawer.jsx
@@ -1,8 +1,14 @@
 import { Button } from "@/components/ui/button";
 import { Drawer, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
+import { getAllMenuDrawerOptions } from "@/lib/action/menu.actions";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
-const MenuDrawer = () => {
+
+const MenuDrawer = async () => {
+ 
+ const services = await getAllMenuDrawerOptions();
+
  return ( 
   <Drawer direction='left'>
    <DrawerTrigger asChild>
@@ -15,10 +21,19 @@ const MenuDrawer = () => {
      <DrawerTitle>
       Menu
      </DrawerTitle>
+     <div className="space-y-1">
+      {services.map((service) => (
+       <Button variant='ghost' className='w-full justify-start' key={service} asChild>
+        <Link href={`/`}>
+         {service}
+        </Link>
+       </Button>
+      ))}
+     </div>
     </DrawerHeader>
    </DrawerContent>
   </Drawer>
-  );
+ );
 }
  
 export default MenuDrawer;
