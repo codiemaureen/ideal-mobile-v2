5c076441f838b7864dd4bb4eeeaa5f7b2dfffcf5
Codiemaureen <codiestover.cs@gmail.com>
2025-08-15 14:19:29 -0400

    Updating service card key

--- components/shared/services/services-list.jsx
+++ components/shared/services/services-list.jsx
@@ -6,7 +6,7 @@ const ServicesList = ({services, limit}) => {
   <div className="my-10">
    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
     {services.map((service) => (
-     <ServiceCard key={service} service={service} />
+     <ServiceCard key={service.name} service={service} />
     ))}
    </div>
   </div>
