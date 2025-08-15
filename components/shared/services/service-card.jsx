6e75aa58e6ab659a6a435308182c86cb34d842f9
Codiemaureen <codiestover.cs@gmail.com>
2025-08-15 14:19:59 -0400

    Adding card details from database

--- components/shared/services/service-card.jsx
+++ components/shared/services/service-card.jsx
@@ -1,16 +1,27 @@
-import { Card, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import Image from "next/image";
 import Link from "next/link";
 
 const ServiceCard = ({service}) => {
   return ( 
-   <Card className="w-full max-w-sm">
-    <CardHeader>
-     <Link href={`/`}>
-      {service}
-     </Link>
-    </CardHeader>
-   </Card>
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+      <Link href={`/services/${service.slug}`}>
+        <h2 className="h2-bold">{service.name}</h2>
+        <Image 
+          src={service.images[0]} 
+          alt={service.slug}
+          width={300} 
+          height={300} 
+          priority={true}
+        />
+      </Link>
+      <CardContent>
+        {service.description}
+      </CardContent>
+      </CardHeader>
+    </Card>
   );
 }
- 
+
 export default ServiceCard;
