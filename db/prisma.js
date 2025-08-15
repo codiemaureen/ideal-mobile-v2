c2ac87fc8f0ae184eedfa73a44dfd17d12a48084
Codiemaureen <codiestover.cs@gmail.com>
2025-08-15 14:14:39 -0400

    removing adapter from prisma

--- db/prisma.js
+++ db/prisma.js
@@ -1,23 +1,14 @@
-import { neonConfig } from '@neondatabase/serverless';
-import { PrismaNeon } from '@prisma/adapter-neon';
-import { PrismaClient } from '../lib/generated/prisma/index.js'
-import ws from 'ws';
+// db/prisma.js
+import { PrismaClient } from '@prisma/client' // or from your custom output path
 
-// Sets up WebSocket connections, which enables Neon to use WebSocket communication.
-neonConfig.webSocketConstructor = ws;
-
-// Instantiate the adapter correctly
-//  (pool is automatically handled with PrismaNeon in prisma 6)
-const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
-
-export const prisma = new PrismaClient({ adapter }).$extends({
+export const prisma = new PrismaClient().$extends({
   result: {
-   service: {
-     price: {
-       compute(product) {
-         return product.price.toString();
-       },
-     },
-   }
-  }
- });
+    service: {
+      price: {
+        compute(service) {
+          return service.price.toString()
+        },
+      },
+    },
+  },
+})
