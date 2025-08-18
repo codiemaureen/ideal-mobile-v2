810a93783caaf669da4f223755bcd54575241b62
Codiemaureen <codiestover.cs@gmail.com>
2025-08-18 15:58:24 -0400

    Adding list style to detail options

--- components/shared/services/service-card.jsx
+++ components/shared/services/service-card.jsx
@@ -1,13 +1,14 @@
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
+import { BubblesIcon } from "lucide-react";
 
 const ServiceCard = ({service}) => {
   return ( 
     <Card className="w-full max-w-sm">
       <CardHeader>
       <Link href={`/services/${service.slug}`}>
-        <h2 className="h2-bold">{service.name}</h2>
+        <h2 className="h2-bold text-2xl text-center py-5">{service.name}</h2>
         <Image 
           src={service.images[0]} 
           alt={service.slug}
@@ -16,8 +17,12 @@ const ServiceCard = ({service}) => {
           priority={true}
         />
       </Link>
-      <CardContent>
-        {service.description}
+      <CardContent className="w-full px-0 py-5">
+          {service.description.map((s, i) => (
+            <li key={i} className="text-xs">
+              {s}
+            </li>
+          ))}
       </CardContent>
       </CardHeader>
     </Card>
