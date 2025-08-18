9fae96516f71e18747c3063a4181f04ddf17a416
Codiemaureen <codiestover.cs@gmail.com>
2025-08-18 17:24:02 -0400

    Adding websocket config settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // your options here
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Avoid Webpack stubbing these optional native deps
+      config.externals.push({
+        bufferutil: 'commonjs bufferutil',
+        'utf-8-validate': 'commonjs utf-8-validate',
+      });
+    }
+    return config;
+  },
 };
 export default nextConfig;
