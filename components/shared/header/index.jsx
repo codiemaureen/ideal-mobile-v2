9413b0ed5adfb650a7e93977270773ceca8df74f
Codiemaureen <codiestover.cs@gmail.com>
2025-08-20 14:49:37 -0400

    Adding home link

--- components/shared/header/index.jsx
+++ components/shared/header/index.jsx
@@ -1,6 +1,7 @@
 import { APP_NAME } from "@/lib/constants";
 import MenuDrawer from "./menu-drawer";
 import Menu from "./menu";
+import Link from "next/link";
 
 
 
@@ -10,9 +11,11 @@ const Header = () => {
       <div className="wrapper flex-between">
         <div className="flex-start group">
           <MenuDrawer />
-          <span className="hidden lg:block font-bold text-3xl ml-3 text-white">
-            {APP_NAME} 🚧 Under Construction v2.0 in progress 🚧
-          </span>
+          <Link href='/'>
+          
+          <span className="hidden lg:block font-bold text-3xl ml-3 text-white text-center">
+            🚧 Under Construction 🚧<br/>{APP_NAME} V2.0 in progress 
+          </span></Link>
         </div>
         <Menu />
       </div>
