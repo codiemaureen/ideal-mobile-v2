0ebb6da8dbe986bbf0e892dd91c957d85003268e
Codiemaureen <codiestover.cs@gmail.com>
2025-08-20 11:42:33 -0400

    Adding carousel to homepage

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,10 +1,14 @@
+import ImageCarousel from "@/components/shared/image-carousel";
 import ServicesList from "@/components/shared/services/services-list";
 import { getAllServices } from "@/lib/action/services.actions";
+import { getCarouselImages } from "@/lib/getCarouselImages";
 
 const HomePage = async () => {
   const services = await getAllServices();
+  const images = await getCarouselImages();
   return ( 
     <div>
+      <ImageCarousel images={images} />
       <ServicesList services={services} limit={2}/>
     </div> 
   );
