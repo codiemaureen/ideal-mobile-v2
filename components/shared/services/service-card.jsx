018268aaf27ab144ef2c5d859cd5418a6a39720c
Codiemaureen <codiestover.cs@gmail.com>
2025-08-20 12:08:23 -0400

    Cleaning up service card

--- components/shared/services/service-card.jsx
+++ components/shared/services/service-card.jsx
@@ -1,32 +1,48 @@
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
-import { BubblesIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Check, CheckIcon } from "lucide-react";
 
-const ServiceCard = ({service}) => {
-  return ( 
-    <Card className="w-full max-w-sm">
-      <CardHeader>
-      <Link href={`/services/${service.slug}`}>
-        <h2 className="h2-bold text-2xl text-center py-5">{service.name}</h2>
-        <Image 
-          src={service.images[0]} 
-          alt={service.slug}
-          width={300} 
-          height={300} 
-          priority={true}
-        />
-      </Link>
-      <CardContent className="w-full px-0 py-5">
-          {service.description.map((s, i) => (
-            <li key={i} className="text-xs">
-              {s}
-            </li>
-          ))}
-      </CardContent>
+const ServiceCard = ({ service }) => {
+  return (
+    <Card className="h-full flex flex-col"> 
+      <CardHeader className="flex justify-center">
+        <Link href={`/services/${service.slug}`}>
+          <h2 className="text-2xl font-bold text-center py-5">{service.name}</h2>
+          <Image
+            src={service.images[0]}
+            alt={service.slug}
+            width={300}
+            height={300}
+            priority
+            className="rounded-lg"
+          />
+        </Link>
       </CardHeader>
+
+      <CardContent className="w-full py-5 flex flex-col gap-4 flex-1"> 
+        <ul className="space-y-2 grow"> 
+          {service.description.map((s, i) => (
+          <li key={i} className="flex items-start gap-2">
+            <Check
+              className="h-4 w-4 mt-1 shrink-0  text-amber-500"
+              strokeWidth={3}
+              aria-hidden="true"
+            />
+            <span>{s}</span>
+          </li>
+          ))}
+        </ul>
+
+        <Button className="mt-auto">
+          <Link href='/schedule-detail'>
+            Schedule
+          </Link>
+        </Button> 
+      </CardContent>
     </Card>
   );
-}
+};
 
 export default ServiceCard;
