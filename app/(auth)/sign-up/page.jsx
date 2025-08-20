bfa42b9172f261e4899168aa3ebb1eccf1abee75
Codiemaureen <codiestover.cs@gmail.com>
2025-08-19 20:34:04 -0400

    adding sign up form

--- app/(auth)/sign-up/page.jsx
+++ app/(auth)/sign-up/page.jsx
@@ -1,7 +1,49 @@
-const SignUpPage = () => {
- return ( 
-  <>Signup Page</>
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+import Image from "next/image";
+import { APP_NAME } from "@/lib/constants";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import SignUpForm from "./sign-up-form";
+
+
+
+export const metadata = {
+  title:'Sign Up'
+}
+
+const SignUpPage = async(props) => {
+  const {callbackUrl} = await props.searchParams
+  const session = await auth();
+
+  if(session){
+    return redirect(callbackUrl || '/');
+  }
+
+  return ( 
+    <div className="w-full max-w-md mx-auto">
+    <Card>
+      <CardHeader className="space-y-4">
+      <Link href="/" className='flex-center'>
+        <Image 
+        src='/images/logo.jpg' 
+        width={100} 
+        height={100} 
+        alt={`${APP_NAME} logo`}
+        className="rounded-full"
+        priority={true}/>
+      </Link>
+      <CardTitle className="text-center">Create Account</CardTitle>
+      <CardDescription className="text-center">
+        Enter Your Information Below to Sign Up
+      </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-4">
+       <SignUpForm />
+      </CardContent>
+    </Card>
+    </div>
   );
 }
- 
+
 export default SignUpPage;
