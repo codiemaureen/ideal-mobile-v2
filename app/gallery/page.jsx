938b2e8ca846dd1f9f551d4845aa43ac7958d069
Codiemaureen <codiestover.cs@gmail.com>
2025-08-22 08:40:13 -0400

    Bug Fix: Moving header from layout  to page file

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -1,13 +1,16 @@
 import BeforeAfterCarousel from "@/components/shared/before-after-img-carousel";
+import Header from "@/components/shared/header";
 import { getPairedBeforeAfterImages } from "@/lib/getPairedBeforeAfterImages";
 
 
 const Gallery = async() => {
- const pairs = await getPairedBeforeAfterImages();
- return ( 
-   <BeforeAfterCarousel pairs={pairs} />
-
+  const pairs = await getPairedBeforeAfterImages();
+  return ( 
+    <>
+      <Header />
+      <BeforeAfterCarousel pairs={pairs} />
+    </>
   );
 }
- 
+
 export default Gallery;
