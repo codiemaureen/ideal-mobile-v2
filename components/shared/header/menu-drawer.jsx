08274d59dfa901a265255ea817e6632bc511c381
Codiemaureen <codiestover.cs@gmail.com>
2025-08-21 20:19:24 -0400

    adding links and accordion links to menu drawer

--- components/shared/header/menu-drawer.jsx
+++ components/shared/header/menu-drawer.jsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
-import { Drawer, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
+import { Drawer, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger, DrawerClose } from "@/components/ui/drawer";
 import { getAllMenuDrawerOptions } from "@/lib/action/menu.actions";
+import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from '@/components/ui/accordion';
 import { MenuIcon } from "lucide-react";
 import Link from "next/link";
 
@@ -9,31 +10,62 @@ const MenuDrawer = async () => {
  
  const services = await getAllMenuDrawerOptions();
 
- return ( 
-  <Drawer direction='left'>
-   <DrawerTrigger asChild>
-    <Button variant='outline'>
-     <MenuIcon />
-    </Button>
-   </DrawerTrigger>
-   <DrawerContent >
-    <DrawerHeader>
-     <DrawerTitle>
-      Menu
-     </DrawerTitle>
-     <div className="space-y-1">
-      {services.map((service) => (
-       <Button variant='ghost' className='w-full justify-start' key={service} asChild>
-        <Link href={`/`}>
-         {service}
-        </Link>
-       </Button>
-      ))}
-     </div>
-    </DrawerHeader>
-   </DrawerContent>
-  </Drawer>
- );
+ services.map((s,i) => (
+  console.log(s.title)
+ ))
+
+  return (
+ <Drawer direction="left">
+      <DrawerTrigger asChild>
+        <Button variant="outline">
+          <MenuIcon />
+        </Button>
+      </DrawerTrigger>
+
+      <DrawerContent className="px-4">
+        <DrawerHeader className="items-start">
+          <DrawerTitle>Menu</DrawerTitle>
+        </DrawerHeader>
+
+        <nav className="pb-4">
+          <div className="space-y-2">
+            {services.map((service) => (
+              <div key={service.link} className="space-y-1">
+                {/* parent */}
+                <DrawerClose asChild>
+                  <Button
+                    variant="ghost"
+                    className="w-full justify-start"
+                    asChild
+                  >
+                    <Link href={service.link}>{service.title}</Link>
+                  </Button>
+                </DrawerClose>
+
+                {/* always show sublinks */}
+                {service.subtitles?.length > 0 && (
+                  <div className="pl-4 space-y-1">
+                    {service.subtitles.map((st) => (
+                      <DrawerClose asChild key={st.link}>
+                        <Button
+                          variant="ghost"
+                          size="sm"
+                          className="w-full justify-start"
+                          asChild
+                        >
+                          <Link href={st.link}>{st.title}</Link>
+                        </Button>
+                      </DrawerClose>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        </nav>
+      </DrawerContent>
+    </Drawer>
+  );
 }
  
 export default MenuDrawer;
