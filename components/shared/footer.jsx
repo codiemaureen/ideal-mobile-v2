d7e03168f435cc43f3f9b7a9a3fc2a31c6bf48c9
Codiemaureen <codiestover.cs@gmail.com>
2026-04-10 13:21:31 -0400

    Typos

--- components/shared/footer.jsx
+++ components/shared/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
  return ( 
   <footer className="border-1">
    <div className="p-5 flex-center">
-    <p>{currentYear} {APP_NAME}. All Right Reserved</p>
+    <p>{currentYear} {APP_NAME}. All Rights Reserved</p>
     </div>
     <div className="pb-5 flex-center">
     <p>Website by <Link href='https://codiegroth.com/'>Codie Groth</Link></p>
