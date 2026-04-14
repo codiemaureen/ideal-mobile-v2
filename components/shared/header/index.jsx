106f23f8fd6439df84671a7a296746e5804f271b
Codiemaureen <codiestover.cs@gmail.com>
2026-04-14 12:30:02 -0400

    Adding Logo

--- components/shared/header/index.jsx
+++ components/shared/header/index.jsx
@@ -2,7 +2,7 @@ import { APP_NAME } from "@/lib/constants";
 import MenuDrawer from "./menu-drawer";
 import Menu from "./menu";
 import Link from "next/link";
-
+import Image from "next/image";
 
 
 const Header = () => {
@@ -10,9 +10,18 @@ const Header = () => {
     <header className='w-full border-b bg-amber-500'>
       <div className="wrapper flex-between">
         <div className="flex-start group">
-          {/* <MenuDrawer /> */}
           <Link href='/'>
-          
+            <Image
+            src="/images/logo.png"
+            alt="Ideal Mobile Detailing"
+            width={150}
+            height={50}
+            priority
+            className="object-contain"
+          />
+          </Link>
+          {/* <MenuDrawer /> */}
+          <Link href='/' className="justify-center">
           <span className="hidden lg:block font-bold text-3xl ml-3 text-white text-center">
             {APP_NAME} 
           </span></Link>
